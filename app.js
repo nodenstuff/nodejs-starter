e25e1453237ac591e99029e69ba7a5d9575473d7
Pavan Kumar Sunkara <pavan.sss1991@gmail.com>
2011-09-25 15:22:59 +0530

    Configure it for cloudfoundry

--- app.js
+++ app.js
@@ -36,5 +36,5 @@ app.get('/', function(req, res){
   });
 });
 
-app.listen(3000);
+app.listen(process.env.VCAP_APP_PORT || 3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
